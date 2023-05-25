6918605602164a64e25d9bbad3f4d197387f9a42
Szymon Wrzesiński <236706@edu.p.lodz.pl>
2023-05-25 11:07:36 +0200

    removed unnecesary file

--- CudaRuntime1/kernel.cu
+++ CudaRuntime1/kernel.cu
@@ -14,14 +14,14 @@ using namespace std;
 
 __global__ void laplacianFilter(unsigned char *srcImage, unsigned char *dstImage, unsigned int width, unsigned int height)
 {
-   int x = blockIdx.x*blockDim.x + threadIdx.x;
-   int y = blockIdx.y*blockDim.y + threadIdx.y;
+   int x = blockIdx.x * blockDim.x + threadIdx.x;
+   int y = blockIdx.y * blockDim.y + threadIdx.y;
 
    //float kernel[3][3] = {0, -1, 0, -1, 4, -1, 0, -1, 0};
-   float kernel[3][3] = { 0, 1, 0, 1, -4, 1, 0, 1, 0 };
-   //float kernel[3][3] = {1, 4, 1, 4, -20, 4, 1, 4, 1};
+   //float kernel[3][3] = { 0, 1, 0, 1, -4, 1, 0, 1, 0 };
+   float kernel[3][3] = {1, 4, 1, 4, -20, 4, 1, 4, 1};
    //float kernel[3][3] = {-1, -1, -1, -1, 8, -1, -1, -1, -1};   
-   if((x>= KERNEL_SIZE /2) && (x<(width- KERNEL_SIZE /2)) && (y>= KERNEL_SIZE /2) && (y<(height- KERNEL_SIZE /2)))
+   if((x >= KERNEL_SIZE /2) && (x < (width - KERNEL_SIZE /2)) && (y >= KERNEL_SIZE /2) && (y < (height - KERNEL_SIZE /2)))
    {
          float sum = 0;
          for(int ky = -KERNEL_SIZE / 2; ky <= KERNEL_SIZE / 2; ky++) {
@@ -41,12 +41,12 @@ void laplacianFilter_GPU_wrapper(const cv::Mat& input, cv::Mat& output)
         const int outputSize = output.cols * output.rows;
         unsigned char *d_input, *d_output;
         
-        cudaMalloc<unsigned char>(&d_input,inputSize);
-        cudaMalloc<unsigned char>(&d_output,outputSize);
+        cudaMalloc<unsigned char>(&d_input, inputSize);
+        cudaMalloc<unsigned char>(&d_output, outputSize);
 
-        cudaMemcpy(d_input,input.ptr(),inputSize,cudaMemcpyHostToDevice);
-        const dim3 block(BLOCK_SIZE,BLOCK_SIZE);
-        const dim3 grid((output.cols + block.x - 1)/block.x, (output.rows + block.y - 1)/block.y);
+        cudaMemcpy(d_input, input.ptr(), inputSize, cudaMemcpyHostToDevice);
+        const dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+        const dim3 grid((output.cols + block.x - 1) / block.x, (output.rows + block.y - 1) / block.y);
 
         cudaEvent_t start, stop;
         cudaEventCreate(&start);
@@ -56,7 +56,7 @@ void laplacianFilter_GPU_wrapper(const cv::Mat& input, cv::Mat& output)
         laplacianFilter<<<grid,block>>>(d_input, d_output, output.cols, output.rows);
         cudaEventRecord(stop);
 
-        cudaMemcpy(output.ptr(),d_output,outputSize,cudaMemcpyDeviceToHost);
+        cudaMemcpy(output.ptr(), d_output, outputSize, cudaMemcpyDeviceToHost);
         cudaFree(d_input);
         cudaFree(d_output);
 
@@ -69,7 +69,6 @@ void laplacianFilter_GPU_wrapper(const cv::Mat& input, cv::Mat& output)
 int main(int argc, char** argv) {
 
     string input_file = "test.jpg";
-
     cv::Mat srcImage = cv::imread(input_file, cv::ImreadModes::IMREAD_UNCHANGED);
     if (srcImage.empty())
     {
